1f78c829141bb861a982b41d38d2003e9299b013
Edmilson Miyasaki <edmilson.miyasaki@ilegra.com>
2016-02-01 17:27:20 -0200

    ASP.NET 5/MVC 6 implementation

---
+++ AntFlow.NET/src/AntFlow.NET/Model/Mula.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntFlow.NET.Model
+{
+    public class Mula
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Profissao { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data { get; set; }
+        public string Origem { get; set; }
+        public string Destino { get; set; }
+    }
+}
