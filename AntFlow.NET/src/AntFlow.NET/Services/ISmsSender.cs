2f04c9764c60c51774e7c22baf68bdb4e78f68d8
Edmilson Miyasaki <edmilson.miyasaki@ilegra.com>
2016-02-01 11:57:50 -0200

    initial commit

---
+++ AntFlow.NET/src/AntFlow.NET/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntFlow.NET.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
