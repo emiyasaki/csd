fa6f018eaffdfcdacbcfe9260210bb64562d3b1c
Edmilson Miyasaki <edmilson.miyasaki@ilegra.com>
2016-02-01 10:57:22 -0200

    fixing top navigation bar

--- antflowjs/server.js
+++ antflowjs/server.js
@@ -7,16 +7,12 @@ var app = express();
 
 var port = process.env.PORT || 29999;
 
-var destinationRouter = require('./src/routes/destinationRoutes');
-
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-
-//app.use('/Destination', destinationRouter);
 
 app.get('/', function (req, res) {
     //Substituir por dados reais
